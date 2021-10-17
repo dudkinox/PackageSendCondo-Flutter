1de44fc57e19f58908924fc331bc51c0aa25c8ec
ศุภณัฐ ชินราช <45527907+dudkinox@users.noreply.github.com>
2021-10-17 17:04:59 +0700

    alert function

---
+++ login_signup/lib/components/alert.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: must_be_immutable
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AlertMessage extends StatelessWidget {
+  AlertMessage(this.Title, this.DetailMessage, this.Path);
+  String Title;
+  String DetailMessage;
+  var Path;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoAlertDialog(
+      title: Text(Title),
+      content: Text(DetailMessage),
+      actions: [
+        CupertinoDialogAction(
+          onPressed: () => {
+            if (Path != null)
+              {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Path)),
+              }
+            else
+              {
+                Navigator.pop(context),
+              },
+          },
+          child: Text("ยอมรับ"),
+        ),
+      ],
+    );
+  }
+}
