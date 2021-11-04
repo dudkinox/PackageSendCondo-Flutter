692060f089625fe206edce38697a29394f8e72da
ศุภณัฐ ชินราช <45527907+dudkinox@users.noreply.github.com>
2021-11-04 17:40:06 +0700

    key store

--- login_signup/ios/Flutter/flutter_export_environment.sh
+++ login_signup/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\CondoFlutterMobileApp\login_signup"
+export "FLUTTER_APPLICATION_PATH=E:\Flutter\CondoFlutter\login_signup"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
